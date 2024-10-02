6189bc13e810d413fcba665dac1c6a4d5385bda1
Dmytro Marilovtsev <dimarilovcev@gmail.com>
2024-10-02 23:32:40 +0300

    Add fix for followLimit

--- Assets/Editor/MyTest.cs
+++ Assets/Editor/MyTest.cs
@@ -9,7 +9,7 @@ public class PlayerStatsTests
     [SetUp]
     public void Setup()
     {
-        // Створюємо об'єкт гравця та додаємо компонент PlayerStats
+        // Cтворюємо об'єкт гравця та додаємо компонент PlayerStats
         player = new GameObject();
         player.AddComponent<PlayerStats>();
 
@@ -170,7 +170,7 @@ public class CameraTests
     public void TestCameraNotFollowBeyondLimit()
     {
         // Встановлюємо межу позиції персонажа
-        float followLimit = 55;
+        cameraFollow.followLimit = 55;
 
         // Початкова позиція персонажа нижче межі
         playerObject.transform.position = new Vector3(50, 0, 0);
